3fe8a23a221f81897312d5a4d3b0b44aed52f406
rafaelBporto <rafaelbporto88@gmail.com>
2022-12-07 21:06:24 -0300

    feat: exercicio 11

---
+++ Prática - Exercícios SQL/11.sql
@@ -0,0 +1,5 @@
+SELECT * FROM clientes WHERE nome = 'Martinha Lima Zambujal';
+SELECT * FROM produtos WHERE nome = 'Fone Topo de Linha';
+SELECT * FROM compras WHERE id_cliente = 5;
+SELECT * FROM produtos WHERE nome = 'Fone Topo de Linha';
+DELETE FROM compras WHERE id = 6;
