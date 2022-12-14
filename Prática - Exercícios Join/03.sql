7c84f9c33a697b90c515fbcb8427156001850d40
rafaelBPorto <85373624+rafaelBPorto@users.noreply.github.com>
2022-12-14 20:43:09 -0300

    Update 03.sql

--- Prática - Exercícios Join/03.sql
+++ Prática - Exercícios Join/03.sql
@@ -1,1 +1,1 @@
-SELECT u.id, u.name, c.name AS course, s.name as schools, e."endDate" FROM users u JOIN educations e ON u.id = e."userId" JOIN schools s ON e."schoolId" = s.id JOIN courses c ON e."courseId" = c.id;
+SELECT e.id, u.name, c.name AS course, s.name as schools, e."endDate", e.status FROM users u JOIN educations e ON u.id = e."userId" JOIN schools s ON e."schoolId" = s.id JOIN courses c ON e."courseId" = c.id WHERE u.id = 30 and e.status = 'finished';
